5169d71b35901c0969da11cf1980dd36ca6c4eed
H. Rüger <hannesrueger@gmx.de>
2021-07-01 12:10:03 +0200

    added ci

---
+++ docker/build.sh
@@ -0,0 +1,7 @@
+cd ../
+npm install
+npx @zeit/ncc build src/index.ts -o ../docker/app
+cd ../docker
+PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
+docker build -t hrueger/agkiosk:v$PACKAGE_VERSION .
+rm -r app

2a03184dc95bcd763c4b80bc336a058eda24fa56
H. Rüger <hannesrueger@gmx.de>
2021-07-01 12:28:20 +0200

    fixed build workflow

--- docker/build.sh
+++ docker/build.sh
@@ -1,6 +1,11 @@
 cd ../
 npm install
-npx @zeit/ncc build index.ts -o ../docker/app
+npx @zeit/ncc build index.ts -o ./docker/app
 cd ./docker
+rm -f layouts
+rm -f partials
+cp -r ../views app/views
+cp -r ../public app/public
 PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/agkiosk:v$PACKAGE_VERSION .
+rm -r app

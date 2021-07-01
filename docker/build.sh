c69d5fd5c694f9d744c298ca037b97dc1a61b001
H. Rüger <hannesrueger@gmx.de>
2021-07-01 12:22:21 +0200

    fixed build

--- docker/build.sh
+++ docker/build.sh
@@ -4,4 +4,3 @@ npx @zeit/ncc build index.ts -o ../docker/app
 cd ./docker
 PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/agkiosk:v$PACKAGE_VERSION .
-rm -r app

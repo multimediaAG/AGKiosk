da3f2d1ea9020f78398135753dccc1e65a3a0948
H. Rüger <hannesrueger@gmx.de>
2021-07-21 10:07:59 +0200

    debug

--- docker/build.sh
+++ docker/build.sh
@@ -9,6 +9,7 @@ cp -r ../dist app
 cp ../package.json app/package.json
 cp -r ../views app/views
 cp -r ../public app/public
+tree
 PACKAGE_VERSION=$(cat ../package.json | grep version | head -1 | awk -F: '{ print $2 }' | sed 's/[",]//g' | tr -d '[[:space:]]')
 docker build -t hrueger/agkiosk:v$PACKAGE_VERSION .
 rm -r app
